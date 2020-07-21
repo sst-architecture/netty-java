b9555c7dbf31e00971b40f0893b2c8e4288b5f79
shanshitao <shanshi_tao@126.com>
2020-07-21 15:57:51 +0800

    20200721

---
+++ src/main/java/netty/io/bio/tomcat/servlet/FirstServlet.java
@@ -0,0 +1,18 @@
+package netty.io.bio.tomcat.servlet;
+
+
+import netty.io.bio.tomcat.http.GPRequest;
+import netty.io.bio.tomcat.http.GPResponse;
+import netty.io.bio.tomcat.http.GPServlet;
+
+public class FirstServlet extends GPServlet {
+
+	public void doGet(GPRequest request, GPResponse response) throws Exception {
+		this.doPost(request, response);
+	}
+
+	public void doPost(GPRequest request, GPResponse response) throws Exception {
+		response.write("This is First Serlvet");
+	}
+
+}
